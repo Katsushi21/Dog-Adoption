6852b4b23e90c65381395d251c9d957eb7ee35fa
Katsushi21 <sheoaspendie@gmail.com>
2021-02-10 12:42:08 +0900

    詳細情報を取得するfetchAsyncGetDetailを書いた

--- frontend/dog_adoption/src/features/dog_data/dog_dataSlice.ts
+++ frontend/dog_adoption/src/features/dog_data/dog_dataSlice.ts
@@ -1,7 +1,7 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 import {RootState} from '../../app/store';
 import axios from "axios";
-import {PROPS_NEWDATA} from "../types";
+import {PROPS_NEWDATA, PROPS_DATA} from "../types";
 
 const adoptionUrlData = `${process.env.REACT_APP_DEV_ADOPTION_URL}adoption/dog_data/`;
 
@@ -39,6 +39,18 @@ export const fetchAsyncNewData = createAsyncThunk("dog_data/post",
         return res.data;
     });
 
+//特定の保護犬のデータを取得する記述
+export const fetchAsyncGetDetail = createAsyncThunk("detail/get",
+    async (detail: PROPS_DATA) => {
+        const res = await axios.get(`adoptionUrlData/${detail.dataId}`, {
+            headers: {
+                "Authorization": `JWT ${localStorage.localJWT}`,
+            },
+        });
+        return res.data;
+    });
+
+
 export const dog_dataSlice = createSlice({
     name: "dog_data",
     initialState: {
@@ -63,6 +75,22 @@ export const dog_dataSlice = createSlice({
                 updated_at: "",
             },
         ],
+        detail: {
+            id: 0,
+            dogName: "",
+            gender: "",
+            age: "",
+            height: "",
+            observations: "",
+            color: "",
+            hair: "",
+            reason_for_arrival: "",
+            photo: "",
+            procedure: "",
+            companyPost: 0,
+            registered_at: "",
+            updated_at: "",
+        }
     },
     reducers: {
         fetchDataStart(state) {
